5275491a26c02b294fff936e301b37c01aa3544c
Ilygos <contact.maxencebeaumont@gmail.com>
2020-09-27 22:46:56 +0200

    Updates

---
+++ precache-manifest.97640bff6d919a2ea784267a6671f452.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd59a40c612006b59458146121aed5a",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "de3b29802cc63f700013",
+    "url": "/portfolio-react/static/css/main.9bbd730f.chunk.css"
+  },
+  {
+    "revision": "00c001d9edbafb63f829",
+    "url": "/portfolio-react/static/js/2.db142acc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio-react/static/js/2.db142acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de3b29802cc63f700013",
+    "url": "/portfolio-react/static/js/main.1eff2809.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  },
+  {
+    "revision": "57a27dbc602d5206d8566b383aaf3427",
+    "url": "/portfolio-react/static/media/Indelible.57a27dbc.otf"
+  },
+  {
+    "revision": "8a64d53c5041db5b7b7b41b2c576d6df",
+    "url": "/portfolio-react/static/media/SYNTH_BG.8a64d53c.jpg"
+  },
+  {
+    "revision": "5fcd6c657ffa8a6791632c705f0944ea",
+    "url": "/portfolio-react/static/media/VCR.5fcd6c65.ttf"
+  },
+  {
+    "revision": "bc5884b28f91bea8159f7e1fdce9f807",
+    "url": "/portfolio-react/static/media/beetle.bc5884b2.png"
+  },
+  {
+    "revision": "b19c6f13938c2a6f3ea142c7c6f38493",
+    "url": "/portfolio-react/static/media/flump.b19c6f13.png"
+  },
+  {
+    "revision": "801a3b77d4c49f0a18d1b59826375db6",
+    "url": "/portfolio-react/static/media/kaihora.801a3b77.jpg"
+  },
+  {
+    "revision": "788b8b4ea6e78a462e1a06c763491bf0",
+    "url": "/portfolio-react/static/media/pacman.788b8b4e.jpg"
+  },
+  {
+    "revision": "6a6621d23f2ceadc40658a58223f93ae",
+    "url": "/portfolio-react/static/media/peachar.6a6621d2.png"
+  },
+  {
+    "revision": "004f5ad5a1dd63f6fa7b7cbe5145c4d7",
+    "url": "/portfolio-react/static/media/writenslash.004f5ad5.png"
+  }
+]);
